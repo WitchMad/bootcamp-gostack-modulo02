3ae5a325aedc1874cc847366dae03a2e6d5a5529
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-15 16:25:19 -0300

    Fixing migration & model User

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import express from 'express'; // With sucrase is possible to use "import"
 import routes from './routes';
 
+import './database';
+
 class App {
   constructor() {
     this.server = express();
