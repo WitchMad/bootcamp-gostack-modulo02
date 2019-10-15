ae72fef083e3fda751bab059b539bfe176d40e87
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-15 17:10:50 -0300

    chore: UserControl added

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,9 @@
 import { Router } from 'express';
-import User from './app/models/User';
+
+import UserController from './app/controllers/UserCrontroller';
 
 const routes = new Router();
 
-routes.get('/', async (req, res) => {
-  const user = await User.create({
-    name: 'Lucas Henrique',
-    email: 'lucasneri.silva@hotmail.com',
-    password_hash: '234125r12ed12de131233e',
-  });
-
-  return res.json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
