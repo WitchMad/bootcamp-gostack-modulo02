6ce798370928b8ea1cebbc8cda93aa586f5402ec
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-16 08:33:25 -0300

    chore: Password hash added

--- src/app/models/User.js
+++ src/app/models/User.js
@@ -1,4 +1,5 @@
 import { Sequelize, Model } from 'sequelize';
+import bcrypt from 'bcryptjs';
 
 class User extends Model {
   static init(sequelize) {
@@ -8,6 +9,7 @@ class User extends Model {
       {
         name: Sequelize.STRING,
         email: Sequelize.STRING,
+        password: Sequelize.VIRTUAL,
         password_hash: Sequelize.STRING,
         provider: Sequelize.BOOLEAN,
       },
@@ -15,6 +17,14 @@ class User extends Model {
         sequelize, // The conection comes likes a second paramether to super
       }
     );
+
+    this.addHook('beforeSave', async user => {
+      if (user.password) {
+        user.password_hash = await bcrypt.hash(user.password, 8);
+      }
+    });
+
+    return this;
   }
 }
 
