b5f1fa311218010d952bd7f27695f514cfaac06c
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-10-19 10:04:04 -0300

    Queue && Available Appointments added

--- src/app/models/Appointment.js
+++ src/app/models/Appointment.js
@@ -1,4 +1,5 @@
 import { Sequelize, Model } from 'sequelize';
+import { isBefore, subHours } from 'date-fns';
 
 class Appointment extends Model {
   static init(sequelize) {
@@ -8,6 +9,19 @@ class Appointment extends Model {
       {
         date: Sequelize.DATE,
         canceled_at: Sequelize.DATE,
+        past: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(this.date, new Date());
+          },
+        },
+        cancelable: {
+          type: Sequelize.VIRTUAL,
+          get() {
+            return isBefore(new Date(), subHours(this.date, 2));
+            // subHours do a subtration in hour on date => subHours(date, many_hours to be subtrated)
+          },
+        },
       },
       {
         sequelize, // The conection comes likes a second paramether to super
