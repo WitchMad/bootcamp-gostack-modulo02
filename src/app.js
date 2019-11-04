4d2f51ac9e9acf8209a6a743fc298b5a47376c65
Lucas Henrique <lucasneri.silva@hotmail.com>
2019-11-04 14:57:38 -0300

    adjusts to support frontend

--- src/app.js
+++ src/app.js
@@ -1,20 +1,21 @@
-import 'dotenv/config';
+import 'dotenv/config'; // Import env config
 
 import express from 'express'; // With sucrase is possible to use "import"
-import 'express-async-errors';
-import Youch from 'youch';
+import 'express-async-errors'; // Catch the error's in async functions
+import Youch from 'youch'; // Give more error details
 import path from 'path';
-import * as Sentry from '@sentry/node';
+import cors from 'cors';
+import * as Sentry from '@sentry/node'; // Service to get error's when app was in production
 import routes from './routes';
 import sentryConfig from './config/sentry';
 
-import './database';
+import './database'; // Import all database configuration
 
 class App {
   constructor() {
     this.server = express();
 
-    Sentry.init(sentryConfig);
+    Sentry.init(sentryConfig); // Init the service sentry
 
     this.middlewares();
     this.routes();
@@ -22,21 +23,23 @@ class App {
   }
 
   middlewares() {
-    this.server.use(Sentry.Handlers.requestHandler());
-    this.server.use(express.json());
+    this.server.use(Sentry.Handlers.requestHandler()); // Watch all aplication
+    this.server.use(cors());
+    this.server.use(express.json()); // Express using json
     this.server.use(
       '/files',
-      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads'))
+      express.static(path.resolve(__dirname, '..', 'tmp', 'uploads')) // Get a URL to images
     );
   }
 
   routes() {
-    this.server.use(routes);
-    this.server.use(Sentry.Handlers.errorHandler());
+    this.server.use(routes); // Use routes
+    this.server.use(Sentry.Handlers.errorHandler()); // Catch any errors in aplication
   }
 
   exceptionHandler() {
     this.server.use(async (err, req, res, next) => {
+      // In development show the error catched more detailed
       if (process.env.NODE_ENV === 'development') {
         const errors = await new Youch(err, req).toJSON();
 
--- src/app/controllers/SessionController.js
+++ src/app/controllers/SessionController.js
@@ -3,6 +3,7 @@ import * as Yup from 'yup'; // Yup makes a data validation
 
 import authConfig from '../../config/auth';
 import User from '../models/User';
+import File from '../models/File';
 
 class SessionController {
   async store(req, res) {
@@ -23,6 +24,13 @@ class SessionController {
 
     const user = await User.findOne({
       where: { email },
+      include: [
+        {
+          model: File,
+          as: 'avatar',
+          attributes: ['id', 'path', 'url'],
+        },
+      ],
     });
 
     if (!user) {
@@ -33,13 +41,15 @@ class SessionController {
       return res.status(401).json({ error: 'Password does not match' });
     }
 
-    const { id, name } = user;
+    const { id, name, avatar, provider } = user;
 
     return res.json({
       user: {
         id,
         name,
         email,
+        provider,
+        avatar,
       },
       // Creates a token session
       token: jwt.sign({ id }, authConfig.secret, {
